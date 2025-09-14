693791ae8c7f6ab5de63593332dac89977aa1ddc
KimHyeonSu <hirobin2001@gmail.com>
2025-09-14 23:48:04 +0900

    Modify index.js listContainer addEventListener

--- index.js
+++ index.js
@@ -12,7 +12,27 @@ form.addEventListener('submit', (event) => {
     createList();
 });
 
-listContainer.addEventListener('click', deleteOrCheck);
+listContainer.addEventListener('click', (event) => {
+    const target = event.target;
+    const actionTarget = target.closest('[data-action]');
+    
+    if(!actionTarget) return;
+    
+    const action = actionTarget.dataset.action;
+    const todoItem = actionTarget.closest('.todo-item');
+
+    switch(action) {
+        case 'check':
+            checkItem(todoItem); // 체크박스 클릭
+            break;
+        case 'text':
+            openCloseSubText(todoItem); // 내용 클릭
+            break;
+        case 'delete':
+            deleteItem(todoItem); // 삭제 버튼 클릭
+            break;
+    }
+});
 
 // 리스트 생성
 // 중복 내용 추가 방지 기능 만들어도 재밌을듯
@@ -32,25 +52,24 @@ function createList() {
     todoInput.value = '';
 }
 
-// 리스트 체크 or 리스트 삭제
-function deleteOrCheck(event) {
-    if(event.target.classList.contains('btn-delete')) {
-        const liToDelete = event.target.closest('li');
-        liToDelete.classList.add('opacity-0', 'scale-95');
-        setTimeout(() => {
-            liToDelete.remove();
-        }, 300);
-        return;
-    }
-    if(event.target.classList.contains('todo-checkbox')) {
-        const li = event.target.closest('li');
-        const unCheckedIcon = li.querySelector('.fa-square');
-        const checkedIcon = li.querySelector('.fa-square-check');
-        const textSpan = li.querySelector('.todo-text');
-        unCheckedIcon.classList.toggle('hidden');
-        checkedIcon.classList.toggle('hidden');
-        textSpan.classList.toggle('text-gray-500');
-        textSpan.classList.toggle('decoration-gray-500');
-        textSpan.classList.toggle('line-through');
-    }
+function checkItem(todoItem) {
+    const unCheckedIcon = todoItem.querySelector('.fa-square');
+    const checkedIcon = todoItem.querySelector('.fa-square-check');
+    const textSummary = todoItem.querySelector('.todo-text');
+    unCheckedIcon.classList.toggle('hidden');
+    checkedIcon.classList.toggle('hidden');
+    textSummary.classList.toggle('text-gray-500');
+    textSummary.classList.toggle('decoration-gray-500');
+    textSummary.classList.toggle('line-through');
+}
+
+function openCloseSubText(todoItem) {
+    return;
+}
+
+function deleteItem(todoItem) {
+    todoItem.classList.add('opacity-0', 'scale-95');
+    setTimeout(() => {
+        todoItem.remove();
+    }, 300);
 }
